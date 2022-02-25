97979c86b460994777b2e859a7d9ab7c2883af90
kramstyles <mark.eke@decagon.dev>
2022-02-25 23:14:34 +0100

    random game version 1.0

--- random_game.py
+++ random_game.py
@@ -1,7 +1,26 @@
 from sys import argv
 from random import randint
 
+
 class RandGame:
     def __init__(self):
-        print('hello')
+        print(self.game)
 
+    @property
+    def game(self):
+        rand_num = randint(1, 100)
+        try:
+            guess = int(input('Input your guess: '))
+        except ValueError:
+            return "Only numbers are allowed"
+        else:
+            counter = 0
+            while guess is not rand_num:
+                counter += 1
+                if guess > rand_num: print("You are too high")
+                elif guess < rand_num: print("You are too Low")
+                try:
+                    guess = int(input("Don't give up. Try again: "))
+                except ValueError:
+                    print("Only numbers allowed! Try again")
+            return f'You won after {counter} guess(es)'
