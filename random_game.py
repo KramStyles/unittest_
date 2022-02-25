ea52f28bc2df70fbcdedd11748ea9529dbe63989
kramstyles <mark.eke@decagon.dev>
2022-02-26 00:20:42 +0100

    edited random_game to work with sys.argv

--- random_game.py
+++ random_game.py
@@ -3,14 +3,16 @@ from random import randint
 
 
 class RandGame:
-    def __init__(self):
+    def __init__(self, num1=1, num2=100):
+        self.num1 = int(num1)
+        self.num2 = int(num2)
         print(self.game)
 
     @property
     def game(self):
-        rand_num = randint(1, 100)
+        rand_num = randint(self.num1, self.num2)
         try:
-            guess = int(input('Input your guess: '))
+            guess = int(input(f'Input your guess from {self.num1} to {self.num2}: '))
         except ValueError:
             return "Only numbers are allowed"
         else:
@@ -24,3 +26,7 @@ class RandGame:
                 except ValueError:
                     print("Only numbers allowed! Try again")
             return f'You won after {counter} guess(es)'
+
+
+if __name__ == '__main__':
+    RandGame(argv[1], argv[2])
